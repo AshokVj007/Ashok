b94bf1e803f4c9e77efb8429ba5ddc8c63e7b10d
Ashok <ashoker0409@gmail.com>
2022-11-13 15:27:44 +0530

    3 methods added

---
+++ src/main/java/com/hexa/GitTask.java
@@ -0,0 +1,16 @@
+package com.hexa;
+
+public class GitTask {
+	private void m1() {
+     System.out.println("Ashok");
+     }
+	
+	private void m2() {
+   System.out.println("Aravi");
+   }
+	
+	private void m3() {
+    System.out.println("Vengat");
+	}
+
+}
